ae0ff72440e4546fba5789e5bc7f4e181e84e44b
swapna <gavhaneswapna@gmail.com>
2021-06-03 11:36:40 +0530

    mongo update

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
 from flask import Flask,request
-import json
-
+import json import pymongo
+from pymongo import MongoClient
+client=MongoClient("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb")
+db = MongoClient['user_login']
 
 app = Flask(__name__)
 
@@ -8,9 +10,10 @@ app = Flask(__name__)
 def login():
     req=request.data
     reqJson=json.loads(req)
-    if reqJson['username']=='swapna'and reqJson['password']=='abcd':
-        return 'user sucessfully logged in'
-    return 'password incorrect'
+    resp=db.user_login.find_one(reqJson)
+    if resp == None:
+        return {'message':'Incorrect username and password'}    
+    return {'message':'user logged in successfully'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8000,debug=True)
